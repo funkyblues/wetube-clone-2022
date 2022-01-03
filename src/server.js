ea6e2bc083670ff42971637ccab283b815af96e5
funkyblues <dnr8874@naver.com>
2022-01-04 02:54:58 +0900

    INTRODUCTION TO EXPRESS - GET Requests

--- src/server.js
+++ src/server.js
@@ -21,3 +21,14 @@ app.listen(PORT, () => console.log(`✔ Server listening on port ${PORT} 🚀`))
 
 // 보통 서버를 시작했다면 localhost를 통해 접근 가능!
 // nodemon을 종료시키면, localhost:4000 은 열리지 않는다. 서버가 실행되었다가 종료되었다는 뜻.
+
+
+// GET은 HTTP method 무슨말일까 (get this page)
+// HTTP: 서버와 소통하는 방법. 서버끼리 소통하는 방법
+// http request : 웹사이트에 접속하고 서버에 정보를 보내는 방법
+// 아무것도 안했지만 브라우저는 자동으로 페이지를 get하려 함
+
+
+
+// 웹사이트에 접속할 때 브라우저는 웹사이트를 request하고 페이지를 가져다준다.
+// 누군가 GET을 요청하면 server는 어떻게 해야하는가? 
