47209aca8383e1dec8e4e6a0ae941d7d413579d0
funkyblues <dnr8874@naver.com>
2022-01-05 02:37:22 +0900

    ROUTERS - URL Parameters part Two

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,18 +4,24 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-// :id는 parameter라고 불림. :이 무조건 필요. (argument, variable 등으로도 불린대 ㅋ 이름 기억 ㄴㄴ)
-// 포인트는 이걸로 url 안에 변수를 포함시킬 수 있음.
-// 이렇게 표시하는 건, express에게 이것이 변수라는 것을 알려주기 위함!
-// 이것을 코드에서 어떻게 엑세스 하는지 see 함수를 통해 설명.
-
-// 왜 :id보다 upload를 위에 뒀을까?
-// :id를 밑에 두면....? (express의 request는 제일 위의 것을 먼저 보게 된다.)
-// express는 :id가 있는 자리에 upload가 있으니 그걸 id라고 생각하게 된다.
-
 // express에게 id가 숫자여야 한다고 말 할수 있을까?
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+
+// REGEX 함.
+// /(nico\w+)/g -> nico로 시작하는 단어 모두 get! (w는 아무 문자 의미) (nicolas, nicomano)
+// Hello my name is nicolas and im 58, my name is also nicomano
+
+// /(\d+)/g (d는 digit) 숫자만 get! (12, 22)
+// /videos/12
+// /videos/lalalalalla
+// /videos/hello
+// /videos/22
+
+// 오 ㄹㅇ임 ㅋㅋㅋ 이젠 upload가 어디에 가든 상관이 없게되었음.
+// 그러나 우리의 DB가 이런 형식이 아니기 때문에, 이걸 쓰지는 않을 것.
+
+// 결론적으로 할 수 있다. id를 이용해서 req.params.id로 불러와야하기 떄문에, id를 남겨둠.
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
