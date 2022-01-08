3bdf34d79a7baec086221c914164b779bff3bac0
funkyblues <dnr8874@naver.com>
2022-01-08 20:13:10 +0900

    TEMPLATES - Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,7 @@
-const fakeUser = {
-  username: "Changuk",
-  loggedIn: true,
-}
-
-export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser: fakeUser});
+export const trending = (req, res) => { 
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return res.render("home", { pageTitle: "Home", videos })
+};
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 
