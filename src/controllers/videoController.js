c1f8272cc14ccb597490d8d6d054a10991754fcd
funkyblues <dnr8874@naver.com>
2022-01-08 19:58:14 +0900

    TEMPLATES - Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,9 @@
-// 템플릿에 변수를 보내기위해, res.render를 호출할 때 변수도 같이 보내주겠다는 뜻.
-// render는 2가지 변수를 받음: view의 이름 / 템플릿에 보낼 변수
-export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+const fakeUser = {
+  username: "Changuk",
+  loggedIn: true,
+}
+
+export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser: fakeUser});
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 
