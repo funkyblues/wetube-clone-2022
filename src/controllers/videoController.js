bb9448552b07a98c132544f1b4426fd3665daeab
funkyblues <dnr8874@naver.com>
2022-01-08 19:29:49 +0900

    TEMPLATES - Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,16 +1,9 @@
-export const trending = (req, res) => {
-  // home.pug를 rendering! -> html로. 그 후 브라우저로 보내줌
-  // 하지만 에러가 남. default cwd가 package.json이 실행된 위치라서...
-  // cwd는 node.js를 실행하는 디렉토리.
-  // 우리의 경우 package.json의 위치.
-  res.render("home");
-}
-export const see = (req, res) => {
-  res.render("watch");
-}
-export const edit = (req, res) => {
-  res.render("edit");
-}
+// 템플릿에 변수를 보내기위해, res.render를 호출할 때 변수도 같이 보내주겠다는 뜻.
+// render는 2가지 변수를 받음: view의 이름 / 템플릿에 보낼 변수
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
+
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
