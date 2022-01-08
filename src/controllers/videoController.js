337d03dac4d84cefe88cc3ec2456351f27cf0e52
funkyblues <dnr8874@naver.com>
2022-01-09 00:21:58 +0900

    MONGODB AND MONGOOSE - Edit Video part Two (복습필요!)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -37,10 +37,30 @@ export const watch = (req, res) => {
 export const getEdit = (req, res) => {
   const id = req.params.id;
   const video = videos[id - 1]; 
-  res.render("edit", { pageTitle:`Editing : ${video.title}`, video  });
+  res.render("edit", { pageTitle:`Editing : ${video.title}`, video });
 }
 export const postEdit = (req, res) => {
+  // 브라우저는 우리가 준 url로 이동할 것이다.
+  // 우리가 어느 비디오를 수정 중인지 알 수 있도록 route로 부터 id 얻어옴.
+  const { id } = req.params;
   
+  // video의 title 변경 사항을 알 수 있도록,
+  // console.log(req.body);
+  // 근데 undefined라고 나옴 ㅋㅋ...
+  
+  // 설정해주어야 할 게 있다. 우리의 express app은 form을 어떻게 다루어야 할 지 모름.
+  // app에게 form을 이해할 수 있도록 해야 함. express method 확인.
+  // express.urlencoded 참조.
+  // edit.pug에서 input을 name="title"로 했어서 {title:"dfasd"} 이렇게 나옴!
+  
+  // 이제 title을 받아올 수 있돠.
+  // const title = req.body.title;
+  const { title } = req.body;
+  
+  // title을 변경해주기 위해! (현재 back-end DB는 없는 상황이므로)
+  videos[id - 1].title = title;
+  // res.redirect()는 브라우저가 redirect(자동으로 이동)하도록 하는 것
+  return res.redirect(`/videos/${id}`);
 }
 
 
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,7 +4,9 @@ import { watch, getEdit, postEdit, upload, deleteVideo } from "../controllers/vi
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", getEdit);
+// handler를 붙여주는 방식으로 적용하면 간단하게 코드 작성 가능.
+// 하나의 url에 get, post 방식을 쓰도록 할 때 아주 유용.
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
 // 이제 우리 서버는 post를 이해하게 된 것.
 videoRouter.post("/:id(\\d+)/edit", postEdit);
