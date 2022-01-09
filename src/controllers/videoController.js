ef94280b7167936cab4728d7f3bbdc8b08cbb37e
funkyblues <dnr8874@naver.com>
2022-01-10 02:23:36 +0900

    MONGODB AND MONGOOSE - Our First Query part Two(1)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,5 @@
 import Video from "../models/Video";
 
-
 export const home = (req, res) => { 
   // Video.find();
   // 사용하기 위한 방식으로는 callback, promise 두 가지가 있다. 이게 뭐임???
@@ -13,11 +12,36 @@ export const home = (req, res) => {
   // 약간의 기다림이 필요하다.
   // 먼저 callback방식으로 사용하는 법 부터.
 
-  Video.find({}, (err, docs) => {});
+
+
+
+  // 예시를 위한 코드!! (callback의 힘: 특정 코드를 마지막에 실행하게 함)
+  console.log("Starting Search");
+  Video.find({}, (error, videos) => {
+    console.log("Finished Search");
+    console.log("errors", error); // errors -> null
+    console.log("videos", videos); // videos -> []
+  });
   // search terms가 {}로 비어있으면 모든 형식을 찾는다는 것을 뜻함.
   // 그 다음으로 callback을 전송. 이 callback은 err, docs라는 signature를 가짐.
 
-  return res.render("home", { pageTitle: "Home" })
+  // callback을 사용하면 아무것도 return 되지 않아야 한다.
+  console.log("hello"); // -> hello가 error, videos보다 먼저 나온다.
+  // 코드 상으로는 error, video가 위에 있으니 error, video, hello가 나와야 하는데 그렇지 않음
+  console.log("I should be the last one");
+  // 정리
+  // 먼저 DB가 연결 -> hello가 출력 -> template을 render -> 그 후 errors와 videos 출력
+  // 오래 걸리지는 않지만 약간의 시간이 걸린다.
+  // (기억: 먼저 /(home) page를 request하고 console.log("hello") 출력. render과정을 거쳐야 logger를 얻게 됨. 
+  // response를 요청해서 받은 후 다시 전송해야 logger에 찍히는것이지. render와 response과정 이후 error, videos를 console.log하는 것이다. )
+
+
+  return res.render("home", { pageTitle: "Home", videos: [] })
+
+
+
+
+
 };
 export const watch = (req, res) => {
   const id = req.params.id;
