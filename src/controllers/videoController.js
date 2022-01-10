63cf940bbce50373274f2503bb0737083668b1fd
funkyblues <dnr8874@naver.com>
2022-01-11 00:12:58 +0900

    MONGODB & MONGOOSE - Creating a Video part Two (1)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,6 +2,8 @@ import Video from "../models/Video";
 
 export const home = async(req, res) => {
   const videos = await Video.find({});
+  // 우리의 video들이 어떻게 생겼을지를 보자
+  console.log(videos);
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
@@ -21,43 +23,58 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video"});
 };
 
-export const postUpload = (req, res) => {
-  // Upload Video의 input값이 body에 있다고 했었지.
-  // console을 보면 잘 출력된 것을 볼 수 있다.
-  // 자, 이제 비디오를 어떻게 만들 수 있을까? 
-  // 그러기 위해선 document를 만들어줘야 한다. -> 데이터를 가진 비디오.
-  // 그 후 document를 database에 저장해주어야 한다.
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  // console.log(title, description, hashtags);
-
-  // Video는 함수가 아니기 때문에, 새로 정의해준다는 의미에서 new 키워드를 붙여야 한다고 함...
   const video = new Video({
+    // 만약 title에 String이 아닌 숫자를 넣으면 어떻게 될까?
+    // 숫자를 넣어도 문제없이 string으로 변환된다
     title,
     description,
     createdAt: Date.now(),
     hashtags: hashtags.split(",").map(word => `#${word}`),
     meta: {
+      // 여기에 string을 넣으면 어떻게 될까??
+      // meta없이 console에 출력된다.
+      // (잘못된 정보를 보냈기 때문!)
+
+      // mongoose에 의해 보호받고 있음! 그러므로 먼저 mongoose에게 데이터 타입을 알려줘야 하는겨
       views: 0,
       rating: 0,
     },
-    // 만약 string에 hashtag가 없는데 추가하고 싶으면 두 가지 function을 이어 실행해주면 된다.
-    // "food,movies,music".split(",").map(word => `#${word}`) -> 이러면 #이 붙어서 나오게 되지.
   });
-  console.log(video);
-  // 확인해보면, Upload Video에서 작성한 title, description등등이 db로 넘어오게 된다!!
-  // 이상한 id 값도 있는데 이는 mongoose가 랜덤으로 아이디를 부여해준 것임.
+  // 그러나 현재 video는 javascript 세계에 있기 때문에, database로 보내줘야 함
+  const dbVideo = await video.save();
+  // save는 promise를 return 해줌 -> 이 말은 save 작업이 끝날때 까지 기다려줘야 한다는 뜻이지
+  // save가 되는 순간 기다려줘야 한다 database에 기록되고 저장되는데에는 시간이 걸리기 때문.
+  // 그러니까 async로 비동기 해주고, await를 video.save()에 적어준다!
+  
+  // save는 promise를 return하고 이걸 await 하면 document가 return 된다.
+  
+  console.log(dbVideo);
+
+  // 잘 저장되어서 화면에 출력되는 것을 볼 수 있다 ㅎㅎ
+
+  // console창으로 가서 mongo활성화 시킨 후, show dbs, use wetube, show collections하면 videos가 나온당
+  // collection: document들의 묶음
+
+  // 데이터를 DB에 저장하는 방식은 두가지 정도임 
+  // 하나는 위에서 한 것 처럼 javascript object를 만들고 object를 database에 저장.
+
+  // 다른 방법은,
+/*export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  await Video.create({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });*/ 
+
+  // 에러가 발생할 수 있기 때문에, try / catch를 사용하자
+
   return res.redirect("/");
 };
-
-
-// hashtags에 대한 여러 케이스를 고려하고 싶은 분들은 startsWith(), trim()을 활용해 보세요 :)
-
-// input : 해시태그1,해시태그#2, #해시태그3, 해시태그4
-
-// hashtags
-// .split(",")
-// .map((word) =>
-// !word.trim().startsWith("#") ? `#${word.trim()}` : word.trim()
-// )
-
-// => ["#해시태그1", "#해시태그#2", "#해시태그3", "#해시태그4#]
