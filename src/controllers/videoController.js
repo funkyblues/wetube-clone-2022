a53feee2c5c9756c18bde65e5863c6a347b2a97c
funkyblues <dnr8874@naver.com>
2022-01-11 00:51:31 +0900

    MONGODB & MONGOOSE - Exceptions and Validation

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -23,58 +23,31 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video"});
 };
 
+// 에러가 발생할 수 있기 때문에, try / catch를 사용하자
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
-    // 만약 title에 String이 아닌 숫자를 넣으면 어떻게 될까?
-    // 숫자를 넣어도 문제없이 string으로 변환된다
-    title,
-    description,
-    createdAt: Date.now(),
-    hashtags: hashtags.split(",").map(word => `#${word}`),
-    meta: {
-      // 여기에 string을 넣으면 어떻게 될까??
-      // meta없이 console에 출력된다.
-      // (잘못된 정보를 보냈기 때문!)
+  try {
+    await Video.create({
+      title,
+      description,
+      // createdAt: Date.now(),
+      // 이렇게 required해둔 코드를 없애면, javascript가 무한반복되면서 멈춤.
+      // return res.redirect가 발동하지 않아서야.
 
-      // mongoose에 의해 보호받고 있음! 그러므로 먼저 mongoose에게 데이터 타입을 알려줘야 하는겨
-      views: 0,
-      rating: 0,
-    },
-  });
-  // 그러나 현재 video는 javascript 세계에 있기 때문에, database로 보내줘야 함
-  const dbVideo = await video.save();
-  // save는 promise를 return 해줌 -> 이 말은 save 작업이 끝날때 까지 기다려줘야 한다는 뜻이지
-  // save가 되는 순간 기다려줘야 한다 database에 기록되고 저장되는데에는 시간이 걸리기 때문.
-  // 그러니까 async로 비동기 해주고, await를 video.save()에 적어준다!
+      // try / catch로 넣어주면, javascript는 try 한 뒤 catch 할 것임
+      // catch가 없으면 서버는 아무것도 안할것이다.
   
-  // save는 promise를 return하고 이걸 await 하면 document가 return 된다.
-  
-  console.log(dbVideo);
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  }
+  catch(error) {
+    // console.log(error);
+    // error가 발생하면 upload 다시 render!
+    // 그러나 사용자는 에러메시지를 직접 볼 수 없는 상태이다.
 
-  // 잘 저장되어서 화면에 출력되는 것을 볼 수 있다 ㅎㅎ
-
-  // console창으로 가서 mongo활성화 시킨 후, show dbs, use wetube, show collections하면 videos가 나온당
-  // collection: document들의 묶음
-
-  // 데이터를 DB에 저장하는 방식은 두가지 정도임 
-  // 하나는 위에서 한 것 처럼 javascript object를 만들고 object를 database에 저장.
-
-  // 다른 방법은,
-/*export const postUpload = async (req, res) => {
-  const { title, description, hashtags } = req.body;
-  await Video.create({
-    title,
-    description,
-    createdAt: Date.now(),
-    hashtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });*/ 
-
-  // 에러가 발생할 수 있기 때문에, try / catch를 사용하자
-
-  return res.redirect("/");
+    // 1. 에러 메시지를 만들어서 upload template으로 보낸다.
+    // Video validation failed가 문자열로 나타난다!
+    return res.render("upload", { pageTitle: "Upload Video", errorMessage: error._message, });
+  }
 };
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,13 +1,20 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: String, //{type: String}
-  description: String, //{type: String}
-  createdAt: Date,
+  title: {type: String, required: true}, //{type: String}
+  description: {type: String, required: true}, //{type: String}
+  createdAt: {type:Date, required: true, default: Date.now },
+  // 그런데 수많은 model에 각각 이렇게 해주는게 끔찍할 수 있다.... 그러니 default를 사용!
+  // Date.now에 ()를 하지 않는 것이 중요. ()해주면 function을 즉각 실행할 것.
+  // 그게 아닌 Mongoose가 알아서 Date.now를 실행해주도록 하기 위함.
+
+  // 이렇게 해주고 실행하면 생성한 video가 createdAt항목을 가지고 있지 않아도
+  // 오류가 발생하지 않고 새로운 video를 생성해준다!
   hashtags: [{type: String}],
   meta: {
-    views: Number,
-    rating: Number,
+    // 이렇게 default를 쓰면 required가 무쓸모다~~
+    views: {type: Number, default: 0, required: true},
+    rating: {type: Number, default: 0, required: true},
   },
 });
 
