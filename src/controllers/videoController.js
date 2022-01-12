92ff851504c72fc512d41a6acaeea298752e736e
Changuk <dnr8874@gmail.com>
2022-01-12 20:48:30 +0900

    MONGODB & MONGOOSE - Edit Video part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -53,9 +53,43 @@ export const getEdit = async (req, res) => {
 };
 
 
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  // const { title } = req.body;
+  // postEdit이 어떤 정보를 받을지 console.log로 확인해보자
+  
+  // console.log(req.body)
+
+  // 일단은 당연히 업데이트 되지 않는다. ㅋㅋ 주소만 redirect해주는 거니까....
+  // 그치만 console창에는 잘 뜬다. 후후. req.body가 무슨 역할을 하는지 기억하자.
+
+  // 수정하기 위한 title, description, hashtags를 req.body로 부터 받자.
+  const { title, description, hashtags } = req.body;
+
+  // 해야할 것은?? -> Database내의 video를 찾아야 한다.
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  // 영상 title, description, hashtags들을 수정.
+  video.title = title;
+  video.description = description;
+  video.hashtags = hashtags.split(",").map((word) => `#${word}`);
+
+  // 그러나 여전히 오류가 날 것이다. 왜냐? 우리가 #을 추가하고 있는데,
+  // 이미 #이 다 달려있기 때문이지. -> hashtags 관련 function들을 손 봐야 한다.
+
+  // 바꾸기 전에, title, description, hashtags들을 일일히 직접 입력해주고 있다.
+  // 지금은 괜찮지만, 영상이 늘어나면 굉장히 귀찮은 일이 될 것이다. 그럼 뭐냐? 함수를 만들어줘야지(맞나?).
+  // 그러나 일단은 그대로 진행.
+  
+  // 우리가 여기서 await를 사용했기 때문에 여기서 에러가 생길 수 있지만 (왜??), 나중에 try catch를 적용하자.
+  await video.save();
+  // 작동은 잘 하고 있다 ㅎㅎ 그러나 postEdit하는 부분은 개선될 여지가 있다.
+
+  // 일단은 해시태그 부. 수정할 때에는 #을 빼주어야 한다. 굉장히 귀찮은 일.
+  // 만약 앞에 #이 있다면 아무것도 추가하지 않을 것임.
+
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
@@ -70,6 +104,8 @@ export const postUpload = async (req, res) => {
       description,
       // hashtags는 현재 잘 돌아가는 코드이지만, 나중에 video를 수정할 때 문제가 생길 거임.
       // 나중에 video를 수정할 때 즈음 해볼거라고 하네. 그때 가서 알려준댕
+
+
       // (이렇게 코드를 복붙하는 게 좋은 방법은 아니다)
       hashtags: hashtags.split(",").map((word) => `#${word}`),
     });
