ea89619b49315bff91cba82bd085f326cda1ee05
funkyblues <dnr8874@naver.com>
2022-01-13 21:27:29 +0900

    MONGODB & MONGOOSE - Middlewares

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -17,7 +17,6 @@ export const watch = async (req, res) => {
   return res.render("watch", { pageTitle: video.title, video});
 };
 
-// getEdit는 object를 edit template으로 보내줘야하기 때문에 video object가 꼭 필요하다.
 export const getEdit = async (req, res) => {
   const id = req.params.id;
   const video = await Video.findById(id);
@@ -28,46 +27,20 @@ export const getEdit = async (req, res) => {
   res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
-// postEdit 에선 video object가 꼭 필요하지 않으므로 Video.exists를 해주면 된다.
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  // 우리의 Video model 안에 있는 video object들은, _id라는 id 값들을 가지고 있다.
-  // exists는 filter를 필요로 함. 
-  // 어떤 속성(property)도 필터가 될 수 있다.
-
   const video = await Video.exists({ _id:id });
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
 
-  // 만듦과 동시에 업데이트 하는 mongoose api
-  // findByIdAndUpdate 문서를 잘 읽을 것.
-
-  // arguments 순서
-  // 1. 업데이트 하고자 하는 영상의 ID.
-  // 2. 업데이트 할 정보 혹은 내용.
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    
-    
-    // hashtags는 영상 업데이트나 영상 생성을 위해서 혹은 업데이트를 위해서, 저장하기 전에 해시태그들을 처리함!
-    // 그러므로 해시태그 부분은 Model에 저장하기 전에 처리하는 것들임
-    // 그리고 Model에 저장하기 전에 여러가지를 먼저 해야하는 것은 비단 hashtags만이 아닐 것이다.
-
-    // 만약 user를 생성한다고 가정하면, 생성하기 전에 email이 존재하는 이메일인지 아닌지 확인해야 한다.
-    // 그러므로 일반적으로 저장하기 전에 고려해야 할 것들이 많음!
-
-    // mongoose에서는 영상을 저장, 혹은 업데이트 전에 이것저것 하라고 시킴
-    // 그것을 Middleware라고 한다. Express에서 본 것과 같이 mongoose에도 있음
-    // pre, post, hook도 있다. (이름은 중요치 않음)
-    // 중요한 것은 얘내들이 끼어들 틈을 만들어 준다는 것.
-    // object가 저장되기 전에 무언가를 하고 나머지를 처리할 수 있도록!!
-
-    // 우리의 경우 hashtags를 처리해서 hashtag같이 보이게 하는 function부분이 될 것임.
-    // 다음 영상에서 해시태그들을 처리하는 Middleware들을 공부하게 될 것이다.
-
+      // 하지만 작성한 middleware는 
+      //  videoUpdate를 위한 것에는 소용이 없을거다.
+      // 뭐가 필요할까?
     hashtags: hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`)),
   });
   
@@ -83,7 +56,12 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map((word) => word.startsWith("#") ? word : `#${word}`),
+      // 이제 hashtags를 변경해보려고 한다. 어떻게 될까??
+      // 이렇게 되면 hashtags는 array가 아닌 string이 된다. hashtags는 그저 문자열이니까
+      hashtags,
+      // 콘솔을 보면 hashtag가 저장되는 법도 다르게 된다. mongoose에서 배열로 설정해서 배열로 들어가긴 하지만, 하나의 문자열로
+      // 입력이 들어간다.
+      // 그럼 이제 hashtags array의 첫 element가 format되는 방식을 설정해보자. #이 들어가야 하니까 -> Video로!
     });
     return res.redirect("/");
   }
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,3 +1,5 @@
+// db안에 영상들 지우는 방법!! : db.videos.remove({}) 이렇게 하면 된다.
+
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
@@ -10,6 +12,31 @@ const videoSchema = new mongoose.Schema({
     rating: {type: Number, default: 0, required: true},
   },
 });
+
+videoSchema.pre("save", async function() {
+  // https://mongoosejs.com/docs/middleware.html
+  // 여기보면 mongoose middleware에 관해서 정말 훌륭한 것이 있다.
+  // function 안에 this 라는 키워드가 있다.
+  // 이 this는 우리가 저장하고자 하는 문서를 가리킨다.
+  // 한번 확인해보자
+
+
+  // console.log("We are about to save: ", this);
+
+
+  // 이렇게 하면 우리가 upload를 통해 생성한 video에 대한 title, description, hashtags 및 meta 등등 데이터가 나온다.
+  // this.title = "Hahaha! I'm a middleware!!!" 
+
+  // 코드를 잘 읽어봐 이해할 수 있음 ㅎㅎ 
+  // 첫 번째 요소가 string이니까 받은 다음에 ,로 나누고  #이 있나 없나 확인 후 다시 배열 할당!
+  this.hashtags = this.hashtags[0].split(",").map((word) => word.startsWith("#") ? word : `#${word}`); // this.hashtags가 array이니까. [0]하면 string이 될 것이다.
+  // 잘 작동한다!
+  // 이것이 pre middleware를 save이벤트에 적용시킨 결과!
+  // 하지만 videoUpdate를 위한 것에는 소용이 없을거다.
+})
+
+
+// 중요! mongoose에선 middleware를 model이 생성되기 전에 만들어야 한다.
 
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
