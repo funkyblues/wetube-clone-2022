85eeb8a91405c9e4c2c8ed5ebb75f792bee1eeaa
funkyblues <dnr8874@naver.com>
2022-01-14 20:30:00 +0900

    USER AUTHENTICATION - Status Codes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,40 +2,32 @@ import User from "../models/User";
 
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-
-
-// postJoin은 아직 기능 x
-// 한번 테스트 해보자
 export const postJoin = async (req, res) => {
-  // console.log(req.body);
-  // res.end();
-  // 아주 잘 작동하고 있음! ㅎ 
   const { name, username, email, password, confirmPassword, location } = req.body;
   const pageTitle = "Join"
 
-  if (password !== confirmPassword) {
-    // return을 붙여줘야 함수를 끝낼 수 있는걸 기억.
-    return res.render("join", { pageTitle, errorMessage: "Password confirmation does not match.",});
-  }
+  // DB에 저장은 안되므로 잘 작동하긴 하지만, 브라우저는 비밀번호를 저장할까요? 하는 창을 띄워버린다.
+  // status code(상태코드)를 알 필요가 있다.
 
-  // 입력한 username이 이미 사용중인지 체크해보자.
-  // const usernameExists = await User.exists({username: username }); //username으로 써도 되는거 알지??
-  // 만약 username이 사용중이면 errorMessage를 보내도록.
-  
+  // https://en.wikipedia.org/wiki/List_of_HTTP_status_codes(참고)
+  // 왜 chrome이 그런 창을 띄웠냐면, 2xx HTTP request를 보냈기 때문. 이것은 성공적인 응답으로 인식하기 때문에,
+  // 그래서 크롬이 계정 생성이 성공적이었다고 판단하게 되는 것. 
+  // 우린 user에게 errorMessage를 잘 보여줬지만, 브라우저는 200을 받았기 때문에 계정 생성이 잘 되었다고 판단하게 되는 것.
+  // 기본적으로 res.render같은걸 하게 되면 상태코드 200을 받게 된다.
+
+  // 그래서 우리는 상태코드 400을 사용할 것. 
+  // 400: Bad request 클라이언트에서 발생한 에러 때문에 요청을 처리 못할 때 쓰는 것
+  // 그럼어떻게 쓸 수 있냐?
+  // res.render() 사이에 status(400)을 추가하면 된다. 그럼 이제 크롬이 비번 저장할까요? 라고 안물을거야.
+
+  if (password !== confirmPassword) {
+    return res.status(400).render("join", { pageTitle, errorMessage: "Password confirmation does not match.",});
+  }
   const exists = await User.exists({$or: [{ username }, { email }]});
   
   if(exists) {
-    return res.render("join", { pageTitle, errorMessage: "This username/email is already taken.",});
+    return res.status(400).render("join", { pageTitle, errorMessage: "This username/email is already taken.",});
   };
-
-  // const emailExists = await User.exists({email});
-  // if (emailExists) {
-  //   return res.render("join", { pageTitle, errorMessage: "This email is already taken.",});
-  // }
-
-  // 지금 return res.render("join", { pageTitle, blabla }) 부분이 반복되고 있음
-  // 이렇게 만들고 싶지 않다면, mongodb의 $or operator를 사용할 수 있음.
-
   await User.create({
     name,
     username,
@@ -43,23 +35,7 @@ export const postJoin = async (req, res) => {
     password,
     location,
   });
-  // 이렇게 계정을 만들어 준 후에 login page로 redirect 시키면 될 거 같다.
   return res.redirect("/login");
-  // 하지만 login page는 아직 "login" 텍스트만 내보내고 있는 것을 알 수 있따.
-  // mongo 콘솔로 접속해서 use wetube, show collections, db.users.find({}) 해주면 
-  // user의 입력 정보들이 잘 나오는 걸 볼 수 있다 ㅎㅎ
-  // 그러나 password가 그대로 보이기 때문에, 보안이 전혀 되지 않는다.
-  // 아주 위험한 것임.
-  // 그러므로 password를 저장하기 전에 보안처리를 해주어야 한다.
-  // 우리의 목표는 이 password를 봐도 해석이 되지 않게 만들어 주는 것.
-  // password hashing!!
-
-  // 입력받은 비밀번호를 hashing할거야. hashing은 일방향 함수임.
-  // 절대 되돌릴 수 없다. 출력값으로 input값을 알 수 없음.
-  // CS에서 이를 deterministic function(결정적 함수)라고 한다.
-
-  // 우리는 bcrypt.js로 password를 해싱할 것이다.
-  // 해커는 해싱된 password를 가지고 할 수 있는 공격이 있는데, rainbow table이라는 게 있다. youtube 에서 nomad채널 확인해봐바
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
