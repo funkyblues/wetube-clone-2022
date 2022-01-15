061c9cf947b83a008ced80afd7f946c0a3f2a326
funkyblues <dnr8874@naver.com>
2022-01-15 18:12:43 +0900

    USER AUTHENTICATION - Expiration and Secrets

--- src/server.js
+++ src/server.js
@@ -15,36 +15,50 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended:true }));
 
+
+
+//undefined라고 뜬다.
+console.log(process.env.COOKIE_SECRET);
+
+
 app.use(
   session({
-    secret: "Hello!", 
-    // resave와 saveUninitialize의 차이점을 알아보자. 
-    // 그리고 이것들을 false로 바꾸는 것에 대해 알아보자.
-    // session authentication을 사용하면서 생길 수 있는 문제에 대해 배울 수 있기 때문.
+    // secret은 우리가 쿠키에 sign할 때 사용하는 string
+    // 쿠키에 sign하는 이유: 우리 backend가 쿠키를 줬다는 것을 보여주기 위해
+    // 왜냐면 session hijack이라는 공격유형이 있어서. 이걸 잘 보호해야 한다. 누군가 나의 쿠키를 훔칠 수 있기 때문이지.
+    // 이 string을 가지고 쿠키를 sign하고 우리가 만든 것임을 증명할 수 있기 때문.
+    // 쿠키의 Domain은 이 쿠키를 만든 backend가 누구인지를 알려줌.
+    
+    // 브라우저는 Domain에 따라 쿠키를 저장하도록 되어 있다. 그리고 쿠키는 Domain에 있는 backend에만 전달된다.
+    // 그래서 우리가 어떤 요청을 하던 간에 쿠키가 localhost로 전송될 것이다. 
+    // 그러니 내 쿠키는 localhost로만 갈 것이야 youtube, instagram 이런데로 안가는 거지 (브라우저가 이 규칙을 지킨다.)
 
-    // 방문한 모든 사람들의 session을 DB에 모두 저장하는데, 그것은 좋은 생각이 아니다.
-    // 아마 로그인한 사용자의 session만 저장하는 것이 좋을 것.
-    // 서버는 로그인 하지 않으면 쿠키를 주지 않고 로그인 하면 쿠키를 넘겨주는 걸로 할 거다.
+    // Path는 그냥 경로(url)
 
-    // resave랑 saveUninitialized를 false로 해보자.
-    // 그러면 이제 sessions db엔 세션이 저장되지 않는다.
-    // uninitialized: 세션이 새로 만들어지고 수정된 적이 없을 때를 말함.
+    // Expires/Max-Age
 
-    // 세션은 어디서 수정할까? -> userController에서!!
+    // 여길 보면 Session이라고 적혀있는데, 이 쿠키는 만료 날짜가 적혀있지 않은 것이다.
+    // 만약 만료 날짜를 지정하지 않으면 이것은 Session cookie가 되고, 사용자가 닫으면 session cookie는 끝나게 된다.
+    // 그러니 브라우저를 끄거나 컴퓨터를 재시작하면 세션이 사라지게 되는거지.
+    
+    // 이건 사용자가 닫지 않는 한 계속 살아있는 거임.
+
+    // 그럼 Max-Age는??
+    // 말 그대로 언제 세션이 만료되는지 알려주는 것. 근데 우리 mongodb의 session에도 exprires라는 속성이 있는데..??
+    // 그래서 브라우저가 평생 켜져있거나 사용자가 브라우저를 계속 사용해도 db내의 session의 기한이 끝나면 사라진다.
+
+    // 우리는 secret과 mongoUrl(DB url)을 보호해야 한다. 다른 사람들이 쓰면 안되거든
+    // 이를 위해 environment file(환경변수)을 만들 것.
+    // 이건 비밀파일이야
+    secret: process.env.COOKIE_SECRET, 
     resave: false,
     saveUninitialized: false,
-    // resave, saveUninitialized: false가 하는 것은, 세션을 수정할 때만 세션을 DB에 저장하고 
-    // 쿠키를 넘겨주는 것. 우리는 로그인 할 때만 세션을 수정하고 있따.
-    // 다시 말하자면, backend가 로그인한 사용자에게만 쿠키를 주도록 설정되었다는 의미!
-
-    // 안드로이드 앱이나 iOS앱을 만들 때 얘내들은 쿠키를 갖지 않기 때문에 token을 사용한다.
-    // 하지만 여기선 브라우저에서 인증을 하니까 쿠키를 이용한 세션인증을 할 수 있다.
-
-    // (물론 브라우저에서 token을 사용해도 되지만 이는 심화내용이라 별개의 공부를 해야함(강의))
-
-    // 이제 우리는 기억하고 싶은 사람들에게만 쿠키를 주게 된다. 바로 우리 유저.
+    cookie: {
+      maxAge: 20000 // 20초후에 쿠키가 사라진다.
+    },
     store: MongoStore.create({
-      mongoUrl: "mongodb://127.0.0.1:27017/wetube"
+      // .env에 접근하는 방법? process.env.DB_URL이렇게 써주면 됨.
+      mongoUrl: process.env.DB_URL
     }),
   })
 );
