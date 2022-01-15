0ee17cb3d61cdefbb10031851278d47e83c01d30
funkyblues <dnr8874@naver.com>
2022-01-15 17:08:43 +0900

    USER AUTHENTICATION - MongoStore

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,10 +1,5 @@
-// db안에 영상들 지우는 방법!! : db.videos.remove({}) 이렇게 하면 된다.
-
 import mongoose from "mongoose";
 import videoRouter from "../routers/videoRouter";
-
-// 이건 이제 필요 없다 
-// export const formatHashtags = (hashtags) => hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
 
 const videoSchema = new mongoose.Schema({
   title: {type: String, required: true, trim: true, maxlength: 80}, 
@@ -16,15 +11,6 @@ const videoSchema = new mongoose.Schema({
     rating: {type: Number, default: 0, required: true},
   },
 });
-// 더 나은것이 있다고 해서 이전의 코드는 지운다.
-// 그러면, 이전에 한 대로 해시태그 만드는 코드를 복붙해야하는데 귀찮고 오류가 생길 수 있는 위험이 있다.
-
-// 해결하기 위한 한 가지 옵션이 있다. 함수를 만드는 것이지. 이것도 나쁜 방법은 아니다. 괜춘.
-// 이렇게 해도 잘 작동된다
-
-// 하지만 Static 이란 것이 있대. 이걸 알려주려고 그랬나봐.
-// https://mongoosejs.com/docs/guide.html
-// Schema.static이랑 function, 만들고자 하는 static의 이름이 필요.
 
 videoSchema.static("formatHashtags", function(hashtags) {
   return hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`));
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -14,33 +15,24 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended:true }));
 
-// 이 middleware는 브라우저가 우리의 벡앤드와 상호작용 할 때 마다 브라우저에 cookie를 전송
-// 브라우저는 cookie로 뭘 할지, 어디에 넣을지 모든걸 알고 있음.
-
-// cookie는 벡엔드가 나의 브라우저에 주는 정보. cookie에는 정해진 규칙이 있기 때문에 매번 backend에 request를 할 때,
-// 브라우저는 알아서 request에 cookie를 덧붙이게 됨.
-
-// cookie에는 무슨 정보가 들어갈까? 
-// 어떤 정보든 넣을 수 있는데, 우리가 넣을 것은 session ID이다.
-// 왜냐면 브라우저와 backend와의 연결이 평생 보장된 것은 없기 때문.
-// 브라우저와 backend간의 연결은 render가 끝나거나, redirect가 발생하거나, 우리가 post request를 보내거나 응답없음을 받으면 connection은 바로 끝난다.
-// 그래서 우리는 사용자에게 session ID를 주는것. 이 ID를 넣을 곳은 바로 cookie!
-
-// cookie랑 session은 별개의 개념.
-// cookie는 단지 backend, frontend간의 정보를 주고받는 방법. 자동적으로 처리되기 때문에 아주 좋음
-// session ID는 cookie 에 저장된다. 왜냐면 cookie는 session ID를 전송하는데 사용되기 때문.
-// 중요한 건 session ID가 cookie안에 저장되고 backend에도 저장된다는 점.
-
-// backend는 생성된 모든 session ID를 관리하는 곳이다.
-// 만약 4명의 사용자가 있다면, session store에 4개의 session이 있는 것.
-// session store: 우리가 session을 저장하는 곳. 매번 코드를 저장하면 서버가 재시작되는데 그러면 session store가 초기화됨.
-// 다음 영상에서 cookie store를 mongodb와 연결할 것임.
-
 app.use(
   session({
     secret: "Hello!", 
     resave: true,
     saveUninitialized: true,
+    // 이제 우리의 세션들은 MongoDB database에 저장되게 된다.
+    // 세션은 어떻게 만들어질까? -> 브라우저가 우리의 backend를 방문할 때 만들어진다.
+    // 새로고침하면 만들어져 있다. 2주동안 기억하겠다는 내용의 expires요소도 있따.
+    // 로그인을 하면, 이미 있던 session에 사용자 정보가 들어간다.
+
+    // 그럼 backend를 껐다가 켜면 어떻게 될까. 똑같지.
+    // 여전히 로그인된 상태로 있다. 로그인 정보는 이제 mongodb에 있기 때문이다.
+    // store: MongoStore 덕분에 mongodb storage에 사용자 세션을 저장하게 되는 것.
+
+    // 이 부분을 없애면 session은 서버의 메모리에 저장된다. 그러면 서버를 재시작할 때 마다 메모리가 지워지므로 세션들을 database안에 저장하도록 한 것이다.
+    store: MongoStore.create({
+      mongoUrl: "mongodb://127.0.0.1:27017/wetube"
+    }),
   })
 );
 
