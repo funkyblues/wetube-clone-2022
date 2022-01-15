973865eea0928b446d338c5056a86f127d631b2d
funkyblues <dnr8874@naver.com>
2022-01-15 16:05:16 +0900

    USER AUTHENTICATION - Logged In User part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -44,17 +44,6 @@ export const postLogin = async (req, res) => {
   if (!ok) {
     return res.status(400).render("login", { pageTitle, errorMessage:"Wrong password" });
   }
-  // 이렇게 하면 세션에 정보를 추가하는 것임!~!
-  // 이렇게 해서 postLogin을 해주면 세션 DB object에 로그인한 유저의 정보가 입력된다~!
-
-  // 세션 DB를 보면 쿠키 id인, 세션 id가 있는걸 볼 수 있다.
-  // loggedIn: true로 되어있고, 유저 정보도 저장되어 있당.
-
-  // 이렇게 화면을 이동할 때 마다 벡엔드에서 유저가 접속해 있다는 것을 알고 있게 된다.
-
-  // 만약 session.loggedIn이 true라면, Join, Login을 없앨 것임.
-  // 그러나 pug에서 잘 안되네. 
-  // 유저가 누구인지는 알지만 그 정보를 pug 템플릿과 공유하지 못하는 상황.
   req.session.loggedIn = true;
   req.session.user = user;
   return res.redirect("/");
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -13,29 +14,34 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended:true }));
 
-app.use(session({
-  secret: "Hello!", 
-  resave: true,
-  saveUninitialized: true,
+app.use(
+  session({
+    secret: "Hello!", 
+    resave: true,
+    saveUninitialized: true,
   })
 );
 
-app.use((req, res, next) => {
-  req.sessionStore.all((error, sessions) => {
-    console.log(sessions);
-    next();
-  })
-});
-app.get("/add-one", (req, res, next) => {
-  req.session.potato += 1;
-  return res.send(`${req.session.id}\n${req.session.potato}`);
-})
+// 접속하는 모든 사람을 보고싶진 않기 때문에, 이제 이 middleware는 지우자.
+//   // // response object를 잠깐 살펴보자.
+//   // // console.log(res);
+//   // // 우리가 지금 관심있는 것은 locals!
+//   // // 왜냐면 pug template에서 locals object 에 접근할 수 있기 때문이지! (pug와 express가 서로 locasl를 공유할 수 있도록 되어 있기 때문)
+//   // // 한번 확인해보자.
+//   // res.locals.sexy = "you";
+//   // // 베리귯!
+//   // res.locals.siteName = "Wetube";
+//   // 이것을 middlewares.js에 저장할 것이다.
+//   // 필요 없게 된 것.
+//   req.sessionStore.all((error, sessions) => {
+//     console.log(sessions);
+//     next();
+//   })
+// });
 
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 export default app;
-
-// 서버가 id를 기억한다고 보긴 힘들다. 1초마다 까먹는 사람이랑 대화하는 것과 같음. 왜냐? 매순간 id를 요구하니까
-// 
